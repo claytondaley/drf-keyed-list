dfdc71142facb1d924341f93ceca431cd460ac6b
Clayton Daley <clayton.daley@gmail.com>
2018-12-05 19:10:37 -0600

    add `__init__` and rename file

---
+++ drf_keyed_list/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'DRF Keyed List'
+__version__ = '0.0.1'
+__author__ = 'Clayton Daley III'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 Clayton Daley'
+
+# Version synonym
+VERSION = __version__
+
+
+from .serializers import KeyedListSerializer
