1a6b858d371bbeaffdf1ebc9082c7adb4d2732d8
Clayton Daley <clayton.daley@gmail.com>
2018-12-07 10:47:46 -0600

    pypi fixes

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
